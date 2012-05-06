cb5caf5e4c779ff8c72ea13155eed40d3f601b28
Tak KUMAGAWA <fragiletoybox@gmail.com>
2012-05-06 20:24:36 +0900

    Added load/post event

--- public/client.js
+++ public/client.js
@@ -1,15 +1,98 @@
 (function() {
+    // page context
+    var context = (function() {
+        var latestId;
+        var oldestId;
+        return {
+            getLatestId: function() {
+                return latestId;
+            },
+            setLatestId: function(id) {
+                latestId = id;
+            },
+            getOldestId: function() {
+                return oldestId;
+            },
+            setOldestId: function(id) {
+                oldestId = id;
+            }
+        };
+    }());
     window.addEventListener('load', function(e) {
         var elComment = document.getElementById('comment');
         clear();
         var ws = new WebSocket("ws://" + document.URL.substr(7).split('/')[0]);
         ws.onopen = function() {
             console.log(new Date() + " Connected");
+            // load latest posts
+            var message = {
+                event: 'refresh'
+            };
+            ws.send(JSON.stringify(message));
         };
         ws.onmessage = function(event) {
-            //            if (event.data) {
-            //                document.getElementById('msg_list').insertBefore(create(event.data), this.firstChild);
-            //            }
+            if (!event.data) {
+                return;
+            }
+            // JSON parse
+            var json = JSON.parse(event.data);
+            console.debug(json);
+            switch (json.event) {
+            case 'refresh':
+                if (json.result == 'SUCCESS') {
+                    // load posts
+                    var tbody1 = document.getElementById('tbody1');
+                    var rs = createMultiRec(json.posts);
+                    for (var i = 0; i < rs.length; i++) {
+                        tbody1.appendChild(rs[i]);
+                    }
+                    // update page context
+                    context.setLatestId(json.latestId);
+                    context.setOldestId(json.oldestId);
+                }
+                else {
+                    console.error('Failed loading posts.');
+                }
+                break;
+            case 'post':
+                if (json.result == 'SUCCESS') {
+                    console.log('Posting comment success');
+                    // load newer posts
+                    ws.send(JSON.stringify({
+                        event: 'getNewer',
+                        data:{
+                            id: context.getLatestId()
+                        }
+                    }));
+                }
+                else {
+                    console.error('Failed posting comment.');
+                }
+                break;
+            case 'getNewer':
+                if (json.result == 'SUCCESS') {
+                    if (json.posts.length === 0) {
+                        console.log('No newer post');
+                        return;
+                    }
+                    // load posts
+                    (function(){
+                        var tbody1 = document.getElementById('tbody1');
+                        var rs = createMultiRec(json.posts);
+                        var before = tbody1.firstChild;
+                        for (var i = 0; i < rs.length; i++) {
+                            tbody1.insertBefore(rs[i], before);
+                        }
+                        // update page context
+                        context.setLatestId(json.latestId);
+                    }());
+                } else {
+                    console.error('Failed loading newer posts.');
+                }
+                break;
+            default:
+                break;
+            }
         };
         ws.onclose = function() {
             console.log(new Date() + " Disconnected");
@@ -27,16 +110,38 @@
             clear();
             return false;
         }, false);
+        document.getElementById('newer').addEventListener('click', function(e) {
+            // load newer posts
+            ws.send(JSON.stringify({
+                event: 'getNewer',
+                data:{
+                    id: context.getLatestId()
+                }
+            }));
+        }, false);
 
         function clear() {
             elComment.value = '';
             elComment.focus();
         }
 
-        function create(data) {
-            var d = document.createElement('div');
-            d.appendChild(document.createTextNode(data));
-            return d;
+        function createSingleRec(post) {
+            var r = document.createElement('tr');
+            var date = document.createElement('td');
+            date.appendChild(document.createTextNode(post.entry_date));
+            var body = document.createElement('td');
+            body.appendChild(document.createTextNode(post.body));
+            r.appendChild(date);
+            r.appendChild(body);
+            return r;
+        }
+
+        function createMultiRec(posts) {
+            var rs = [];
+            for (var i = 0; i < posts.length; i++) {
+                rs.push(createSingleRec(posts[i]));
+            }
+            return rs;
         }
     }, false);
 }());
--- server.js
+++ server.js
@@ -8,9 +8,12 @@ var http = require('http'),
     ejs = require('ejs'),
     StaticServer = require('node-static').Server,
     WebSocketServer = require('websocket').server;
+
 var dba = require('./lib/dba'),
     config = require('./lib/config');
+
 var staticServer = new StaticServer('./public');
+
 var server = http.createServer(function(req, res) {
     if (url.parse(req.url).pathname != '/') {
         // delegate task to static server
@@ -19,29 +22,12 @@ var server = http.createServer(function(req, res) {
         });
         return;
     }
-    // get posts list
-    dba.getInitPosts(function(err, results) {
-        var data = {};
-        if (err) {
-            console.error(err);
-            return;
-        }
-        data.event = 'refresh';
-        data.latestId = results[0].id;
-        data.oldestId = results[results.length - 1].id;
-        data.posts = results;
-        util.debug(JSON.stringify(data));
-        // render html
-        var template = fs.readFileSync(__dirname + '/view/index.ejs');
-        // var params = extractParams(req.url);
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.write(ejs.render(template.toString(), {
-            "data": data
-        }));
-        res.end();
+    var template = fs.readFileSync(__dirname + '/view/index.ejs');
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
     });
+    res.write(ejs.render(template.toString(), {}));
+    res.end();
 });
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
@@ -53,47 +39,78 @@ wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
-        console.log(message);
         var json = JSON.parse(message.utf8Data);
-        util.debug(json);
-        switch(json.event) {
-            case 'post':
-                // post a comment
-                dba.entryPost(json.data, function(err) {
-                    if(err){
-                        console.error(err);
-                        sendReply({
-                            event: 'post',
-                            result: 'FAIL'
-                        });
-                        return;
-                    }
+        util.debug('Received message : '+json);
+        switch (json.event) {
+        case 'refresh':
+            dba.getInitPosts(function(err, results) {
+                var data = {};
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                data.event = 'refresh';
+                data.result = 'SUCCESS';
+                data.latestId = results[0].id;
+                data.oldestId = results[results.length - 1].id;
+                data.posts = results;
+                util.debug('Reply message : '+JSON.stringify(data));
+                sendReply(data);
+            });
+            break;
+        case 'post':
+            // post a comment
+            dba.entryPost(json.data, function(err) {
+                if (err) {
+                    util.debug('Post failed');
+                    console.error(err);
                     sendReply({
                         event: 'post',
-                        result: 'SUCCESS'
+                        result: 'FAIL'
                     });
+                    return;
+                }
+                util.debug('Post Success');
+                sendReply({
+                    event: 'post',
+                    result: 'SUCCESS'
                 });
-                break;
-            case 'getNewer':
-                // load newer posts
-                break;
-            case 'getOlder':
-                // load older posts
-                break;
-            default:
-                console.error('Property "event" is not valid:' + json.event );
+            });
+            break;
+        case 'getNewer':
+            // load newer posts
+            dba.getNewerPosts(json.data, function(err, results) {
+                if (err) {
+                    util.debug('getNewer failed');
+                    console.error(err);
+                    sendReply({
+                        event: 'getNewer',
+                        result: 'FAIL'
+                    });
+                    return;
+                }
+                util.debug('getNewer success');
+                var data = {
+                    event: 'getNewer',
+                    result: 'SUCCESS',
+                    latestId: (results.length > 0) ? results[0].id : null,
+                    posts: results
+                };
+                util.debug('Reply message : '+JSON.stringify(data));
+                sendReply(data);
+            });
+            break;
+        case 'getOlder':
+            // load older posts
+            break;
+        default:
+            console.error('Property "event" is not valid:' + json.event);
         }
-
-//        if (message.type === 'utf8') {
-        //            console.log('Received Message: ' + message.utf8Data);
-        //            wsServer.broadcastUTF(message.utf8Data);
-        //            // connection.send(message.utf8Data);
-        //            console.log('Message sent successfully.');
-        //        }
     });
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
+
     function sendReply(json) {
         connection.send(JSON.stringify(json));
     }
